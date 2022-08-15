076ad3b05b67265dcbac04d747124a7cb3fa7913
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-08-15 17:56:20 +0900

    Set React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,29 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
 import routes from "./routes";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path={routes.home}
-            element={isLoggedIn ? <Home /> : <Login />}
-          />
-          {!isLoggedIn ? (
-            <Route path={routes.signUp} element={<SignUp />} />
-          ) : null}
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path={routes.home}
+              element={isLoggedIn ? <Home /> : <Login />}
+            />
+            {!isLoggedIn ? (
+              <Route path={routes.signUp} element={<SignUp />} />
+            ) : null}
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
 
