48c18b1d38b5c2ee4dce75d46f71c922620d5b23
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-08-17 14:33:20 +0900

    Parsing Hashtags

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,9 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 interface CommentProps {
@@ -15,7 +25,19 @@ function Comment({ author, payload }: CommentProps) {
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>
+        {payload?.split(" ").map((word, index) =>
+          /#[a-zA-Z0-9가-힇ㄱ-ㅎㅏ-ㅣぁ-ゔァ-ヴー々〆〤一-龥\w]+/g.test(
+            word
+          ) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
