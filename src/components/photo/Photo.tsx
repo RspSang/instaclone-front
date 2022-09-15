7d24245b887f79f0a6c744f58a776135ca0f91a6
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-09-15 12:28:04 +0900

    Update Comments

--- src/components/photo/Photo.tsx
+++ src/components/photo/Photo.tsx
@@ -1,91 +1,143 @@
-import { Link } from "react-router-dom";
+import React from "react";
 import styled from "styled-components";
-import Avatar from "../shared/Avatar";
-import Comments from "../feed/Comments";
+import PhotoIcons from "./PhotoIcons";
+import PhotoDetail from "./PhotoDetail";
+import { AnimatePresence } from "framer-motion";
+import {
+  Link,
+  useMatch,
+  PathMatch,
+  useNavigate,
+  NavigateFunction,
+} from "react-router-dom";
+import PhotoAuthor from "./PhotoAuthor";
+import PhotoImage from "./PhotoImage";
+import TotalLikes from "../shared/TotalLikes";
 import Username from "../shared/Username";
-import PhotoActions from "./PhotoActions";
+import CreatedAt from "../shared/CreatedAt";
+import CommentForm from "../comments/CommentForm";
+import CommentsContainer from "../comments/CommentsContainer";
+import TotalComments from "../shared/TotalComments";
 
-export interface IComment {
-  id: number;
-  payload: string;
-  isMine: boolean;
-  createdAt: string;
-  user: {
-    username: string;
-    avatar?: string | null;
-  };
-}
-
-interface PhotoProps {
+interface PhotoContainerProps {
   id?: number;
-  user?: { username: string; avatar?: string | null } | null;
+  user?: {
+    username: string;
+    firstName: string;
+    lastName?: string | null;
+    avatar?: string | null;
+  } | null;
   file?: string;
-  caption?: string | null;
+  isLiked?: boolean;
   likes?: number;
   commentNumber?: number;
-  comments?: (IComment | null)[] | null;
+  caption?: string | null;
+  comments?: any;
   createdAt?: string;
-  isMine?: boolean;
-  isLiked?: boolean;
 }
 
-const PhotoContainer = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  border-radius: 4px;
+const Container = styled.div`
   border: 1px solid ${(props) => props.theme.borderColor};
-  margin-bottom: 60px;
-  max-width: 615px;
+  background-color: ${(props) => props.theme.bgContainerColor};
+  margin-bottom: 20px;
 `;
 
-const PhotoHeader = styled.div`
-  padding: 15px;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid rgb(239, 239, 239);
-`;
-
-const PhotoFile = styled.img`
-  min-width: 100%;
-  max-width: 100%;
-`;
-
-const PhotoData = styled.div`
+const CaptionContainer = styled.div`
   padding: 12px 15px;
+  padding-top: 5px;
+  padding-bottom: 10px;
+  overflow: hidden;
 `;
 
-function Photo({
+const Caption = styled.span`
+  margin-left: 5px;
+  line-height: 1.2;
+  a {
+    color: ${(props) => props.theme.hashtagColor};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const PhotoContainer = ({
   id,
   user,
   file,
   isLiked,
   likes,
-  caption,
   commentNumber,
+  caption,
   comments,
-}: PhotoProps) {
+  createdAt,
+}: PhotoContainerProps) => {
+  const navigate: NavigateFunction = useNavigate();
+  const photoPathMath: PathMatch<"id"> | null = useMatch("/photos/:id");
+
+  const handleOpenPhotoDetail = (id: number | undefined): void => {
+    navigate(`/photos/${id}`);
+  };
+
   return (
-    <PhotoContainer key={id}>
-      <PhotoHeader>
-        <Link to={`/users/${user?.username}`}>
-          <Avatar avatarUrl={user?.avatar} size="28px" />
-        </Link>
+    <Container>
+      <AnimatePresence>
+        {photoPathMath && photoPathMath.params.id === String(id) && (
+          <PhotoDetail
+            id={id}
+            user={user}
+            file={file}
+            isLiked={isLiked}
+            likes={likes}
+            caption={caption}
+            createdAt={createdAt}
+          />
+        )}
+      </AnimatePresence>
+      <PhotoAuthor
+        name={user?.firstName + " " + user?.lastName}
+        username={user?.username}
+        avatarUrl={user?.avatar}
+      />
+      <PhotoImage
+        file={file}
+        handleSeePhotoDetail={() => handleOpenPhotoDetail(id)}
+      />
+      <PhotoIcons
+        id={id}
+        isLiked={isLiked}
+        handleSeePhotoDetail={() => handleOpenPhotoDetail(id)}
+      />
+      <TotalLikes photoId={id} likes={likes} />
+      <CaptionContainer>
         <Link to={`/users/${user?.username}`}>
           <Username username={user?.username} size="15px" />
         </Link>
-      </PhotoHeader>
-      <PhotoFile src={file} />
-      <PhotoData>
-        <PhotoActions id={id} isLiked={isLiked} likes={likes} />
-        <Comments
-          photoId={id}
-          author={user?.username}
-          caption={caption}
-          commentNumber={commentNumber}
-          comments={comments}
-        />
-      </PhotoData>
-    </PhotoContainer>
+        <Caption>
+          {caption?.split(" ").map((word: string, index: number) =>
+            /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g.test(word) === true ? (
+              <Link key={index} to={`/hashtags/${word.replace("#", "")}`}>
+                {word}{" "}
+              </Link>
+            ) : /@[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g.test(word) === true ? (
+              <Link key={index} to={`/users/${word.replace("@", "")}`}>
+                {word}{" "}
+              </Link>
+            ) : (
+              <React.Fragment key={index}>{word} </React.Fragment>
+            )
+          )}
+        </Caption>
+      </CaptionContainer>
+      <TotalComments
+        commentNumber={commentNumber}
+        handleOpenPhotoDetail={() => handleOpenPhotoDetail(id)}
+      />
+      <CommentsContainer photoId={id} comments={comments} />
+      <CreatedAt createdAt={createdAt} />
+      <CommentForm photoId={id} position="bottom" />
+    </Container>
   );
-}
+};
 
-export default Photo;
+export default PhotoContainer;
